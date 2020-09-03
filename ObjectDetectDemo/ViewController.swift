dcc3a95996636e0186787979bf980d0b4662014f
neil-marcellini <neil.marcellini@gmail.com>
2020-09-02 20:57:28 -0700

    Initial Commit

---
+++ ObjectDetectDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ObjectDetectDemo
+//
+//  Created by Neil Marcellini on 9/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
